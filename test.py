fecbd9d9451da48a396cc1222c9596cb124f670c
liang <liangliangz6v6@gmail.com>
2025-03-04 14:22:45 -0500

    update

--- test.py
+++
@@ -1,8 +0,0 @@
-from ogb.nodeproppred import PygNodePropPredDataset
-
-dataset = PygNodePropPredDataset(root='data',name = 'ogbn-proteins') 
-
-split_idx = dataset.get_idx_split()
-train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
-graph = dataset[0]
-print(graph.y[0])
